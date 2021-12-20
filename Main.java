894d010cc1c2c1fe7da04611aa016a05d8f7c145
YaphetYonas1 <85519133+YaphetYonas5@users.noreply.github.com>
2021-12-20 02:59:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    // To hold data.
+      double x, y, result;
+
+      x = 16;
+      y = 2.0;
+
+      // Get the power.
+      result = Math.pow(x, y);
+
+      // Display result.
+      System.out.println(result);
+
+      // Get square root.
+      result = Math.sqrt(x);
+
+      // Display result.
+      System.out.println(result);
+   }
+
+  }
